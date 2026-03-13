07923401cbe451b6476068040d162659cb701575
vivan0788 <alonevivanbabu@gmail.com>
2026-03-13 11:40:31 +0530

    Change MongoDB URI for connection
    Updated MongoDB URI for improved connection settings.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 app = Flask(__name__)
 
 # Aapka MongoDB link jo humne pehle nikala tha
-MONGO_URI = "mongodb+srv://avinash0788:Avinash8@vanx-tracker.qkdovs.mongodb.net/?appName=VanX-Tracker"
+MONGO_URI = "mongodb://avinash0788:@Avinash8@ac-qkdovs-shard-00-00.qkdovs.mongodb.net:27017,ac-qkdovs-shard-00-01.qkdovs.mongodb.net:27017,ac-qkdovs-shard-00-02.qkdovs.mongodb.net:27017/?ssl=true&replicaSet=atlas-9x02k7-shard-0&authSource=admin&retryWrites=true&w=majority&appName=VanX-Tracker"
 client = MongoClient(MONGO_URI)
 db = client['vanx_db']
 collection = db['location_logs']
