b6d053300a96b0a87e21550a14785bdb497ba6d0
vivan0788 <alonevivanbabu@gmail.com>
2026-03-13 15:07:35 +0530

    Use variable for PORT in app.run
    Refactored app.run to use a variable for port.

--- app.py
+++ app.py
@@ -114,4 +114,6 @@ def get_data():
     return jsonify(locations)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    # Render hamesha 'PORT' environment variable ka use karta hai
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
